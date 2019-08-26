ba28416ee307f80fee97a86b8cb4a318a4f06dd7
haaris <hhussain@xula.edu>
2019-08-26 11:04:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hello/Hello.java
@@ -0,0 +1,8 @@
+package com.example.hello;
+
+public class Hello {
+    public static void main(String[] args) {
+        // Hello is a class that merely outputs a welcome message.
+        // Haaris Hussain, SonofHarry2019, 08/22/2019
+    }
+}
