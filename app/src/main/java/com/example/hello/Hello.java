162ad442b87b0b17c54820315c3687a5c58536e1
haaris <hhussain@xula.edu>
2019-08-27 18:00:45 -0500

    Initial commit

--- app/src/main/java/com/example/hello/Hello.java
+++ app/src/main/java/com/example/hello/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         // Hello is a class that merely outputs a welcome message.
         // Haaris Hussain, SonofHarry2019, 08/22/2019
+        System.out.printf("\nHello, DS Lab #1 - Setup Software!\n");
     }
 }
